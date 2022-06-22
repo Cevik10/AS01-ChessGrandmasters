c738010969283c9cafd356cd83b84bc192fb3a31
Hakan Çevik <hakanxcevik7@gmail.com>
2022-06-22 09:59:38 +0300

    Introduction to Android.

--- app/src/main/java/com/hakancevik/chessgrandmasters/MainActivity.java
+++ app/src/main/java/com/hakancevik/chessgrandmasters/MainActivity.java
@@ -2,23 +2,42 @@ package com.hakancevik.chessgrandmasters;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ImageView;
+
+
+import com.hakancevik.chessgrandmasters.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+
+    boolean isOpen = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
     }
 
 
-    public void changeImage(View view){
+    @SuppressLint("SetTextI18n")
+    public void changeImage(View view) {
 
-        ImageView imageView = findViewById(R.id.imageView);
-        imageView.setImageResource(R.drawable.kasparov);
+        if (isOpen) {
+            binding.imageView.setImageResource(R.drawable.kasparov);
+            isOpen = false;
+            binding.nameText.setText("Garry Kasparov");
+        }
+        else {
+            binding.imageView.setImageResource(R.drawable.carlsen);
+            isOpen = true;
+            binding.nameText.setText("Magnus Carlsen");
+        }
+
 
     }
 
